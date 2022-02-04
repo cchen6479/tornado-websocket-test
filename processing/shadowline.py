0dbdacd350a9c3c6e776eed6adba3b96bd49da58
Chris Chen <chrisc48@nycstudents.net>
2022-02-04 17:21:33 -0500

    removed erratic shadow line identification

--- processing/shadowline.py
+++ processing/shadowline.py
@@ -1,25 +1,13 @@
 import cv2
-from processing.constants import camera
 import math
+import numpy as np
 
-cap = cv2.VideoCapture(camera, cv2.CAP_DSHOW)
+from constants import camera
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_EXPOSURE, -7)
 
 MIN_AREA = 1000
-
-# cv2.namedWindow("canny-ing")
-# def findline(val):
-#     pass
-
-# cv2.createTrackbar("low thresh", "canny-ing", 50, 100, findline)
-# cv2.createTrackbar("high thresh", "canny-ing", 50, 100, findline)
-
-# def canny_edge(frame):
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-#     canny = cv2.Canny(blur, cv2.getTrackbarPos("low thresh", "canny-ing"), cv2.getTrackbarPos("high thresh", "canny-ing"))
-#     _, mask = cv2.threshold(canny, 70, 255, cv2.THRESH_BINARY)
-#     return mask
 
 def sobel_edge(frame):
     ddepth = cv2.CV_16S
@@ -48,10 +36,11 @@ def laplace_edge(frame):
     abs_dst = cv2.convertScaleAbs(dst)
     return abs_dst
 
+# criteria for rectangle
 def isRect(cnt, approx, ar):
-    return len(approx) == 4 and cv2.contourArea(cnt) > MIN_AREA and not (0.8 <= ar <= 1.2)
+    return 4 <= len(approx) <= 6 and cv2.contourArea(cnt) > MIN_AREA and not (0.6 <= ar <= 1.4)
 
-#bgr mask and to gray
+# bgr mask and to gray
 def maskColor(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     mask = cv2.inRange(frame, (0, 0, 0), (50, 50, 50))
@@ -59,30 +48,63 @@ def maskColor(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return frame
 
+# draws rectangle and line that transverses through it
 def findRect(frame, output):
     contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for(index, contour) in enumerate(contours):
+    for contour in contours:
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
         x, y, w, h = cv2.boundingRect(approx)
         aspect_ratio = w/h
 
         if isRect(contour, approx, aspect_ratio):
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(output, [box], 0, (0, 255, 0), 2)
+            p1, p2 = get_longest_line(box)
+            cv2.line(output, p1, p2, (255, 0, 0), 2)
+            cv2.putText(output, f"{str(get_angle(p1, p2))} degrees", (int(x), int(y-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,255)  )
+            
+            
+            # cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0))
+            
+            # _,cols = frame.shape[:2]
+            # [vx,vy,x,y] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
+            # lefty = int((-x*vy/vx) + y)
+            # righty = int(((cols-x)*vy/vx)+y)
+            # p1 = (cols-1,righty)
+            # p2 = (0, lefty)
+            # cv2.line(output,p1,p2,(0,0,255),2)
 
-            cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0))
+            # cv2.putText(output, f"{str(get_angle(p1, p2))} degrees", (int(x), int(y-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255,255)  )
 
-            _,cols = frame.shape[:2]
-            [vx,vy,x,y] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
-            lefty = int((-x*vy/vx) + y)
-            righty = int(((cols-x)*vy/vx)+y)
-            p1 = (cols-1,righty)
-            p2 = (0, lefty)
-            cv2.line(output,p1,p2,(0,0,255),2)
-            print(get_angle(p1, p2))
-            cv2.putText(output, f"{str(get_angle(p1, p2))} degrees", (int(x), int(y-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255,255)  )
+def get_longest_line(box):
+    midpoints = [get_midpoint(box[i], box[(i + 1) % 4]) for i in range(4)]
+    d1 = get_distance(midpoints[3], midpoints[1])
+    d2 = get_distance(midpoints[0], midpoints[2])
 
+    if d1 > d2:
+        return (midpoints[3], midpoints[1])
+    else:
+        return (midpoints[0], midpoints[2])
+    
+
+def get_midpoint(p1, p2):
+    x = (p1[0] + p2[0])/2
+    y = (p1[1] + p2[1])/2
+    return(int(x), int(y))
+
+def get_distance(p1, p2):
+    return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
+
+# retrieves angle from slope
 def get_angle(p1, p2):
-    m = (p1[1] - p2[1])/(p1[0] - p2[0])
+    dy = (p1[0] - p2[0])
+    if dy != 0:
+        m = (p1[1] - p2[1])/dy
+    else:
+        m = 0
     return format(math.atan(m) * -180 / math.pi, '.2f')    
 
 def detect_line(frame):
